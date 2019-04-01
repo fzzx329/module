8de45ff925d7c9195aca0c8dce39a8c7f17ced47
fzzx329 <qwescy13690@163.com>
2019-04-01 10:30:05 +0800

    2019-4-1

---
+++ lbt/lbt.vue
@@ -0,0 +1,165 @@
+<template>
+	<div id="lbt" v-bind:style="{width:width+'px',height:height+'px'}" v-on:mouseover="stop()" v-on:mouseout="autoPlay()">
+		
+			<!-- <ul class="tpk" v-bind:style="{width:W,height:height+'px',left:-width+'px'}" v-if="dj">
+				<li>
+					<a href="">
+						<img v-bind:src="tp[tp.length-1]" v-bind:style="{width:width+'px',height:height+'px'}">
+					</a>
+				</li>
+				<li v-for="(img,index) in tp" :key="index">
+					<a href="#">
+						<img v-bind:src="img" v-bind:style="{width:width+'px',height:height+'px'}">
+					</a>
+				</li>
+				<li>
+					<a href="">
+						<img v-bind:src="tp[0]" v-bind:style="{width:width+'px',height:height+'px'}">
+					</a>
+				</li>
+			</ul> -->
+		<transition-group name="dh">
+					<a class="tpa" href="#" v-for="(img,index) in tp" :key="index" v-show="index==cs">
+						<img v-bind:src="img" v-bind:style="{width:width+'px',height:height+'px'}">
+					</a>
+		</transition-group>
+		<ul class="xdk">
+			<li v-for="(img,index) in tp" :key="index" v-on:click="xzPlay(index)" v-bind:class="{'up':index===cs}"></li>
+		</ul>
+		<div class="left" v-on:click="leftPlay">《</div>
+		<div class="right" v-on:click="rightPlay">》</div>		
+	</div>
+</template>
+<style type="text/css">
+	#lbt{
+		margin:0 auto;
+		position: relative;
+		overflow: hidden;
+	}/*外面大框*/
+	#lbt ul{
+		margin: 0;
+		list-style: none;
+		overflow: hidden;
+	}/*初始化ul*/
+/*	.tpk{
+		position: absolute;
+		left: 0;
+		top: 0;
+	}
+	.tpk li{
+		float: left;
+	}
+	.tpk img{
+		vertical-align: top;
+	}*/
+	.tpa{
+		position: absolute;
+		left: 0;
+		top: 0;
+	}
+	.tpa img{
+		vertical-align: top;
+	}
+	.xdk{
+		padding: 5px;
+		position: absolute;
+		left: 50%;
+		bottom: 10px;
+		transform: translateX(-50%);
+	}/*小点ul 自动水平居中*/
+	.xdk li{
+		margin: 0 6px;
+		float: left;
+		height: 10px;
+		width: 10px;
+		border: 2px solid #fff;
+		border-radius: 50%;
+		background-color: #fff;
+		cursor:pointer;	
+	}/*小点*/
+	.xdk li:hover{
+		background-color: #999;
+	}/*小点hover效果*/
+	.up{
+		background-color: #888 !important;
+		border-color: #888 !important;
+	}/*小点变色*/
+	.left,.right{
+		position: absolute;
+		top: 50%;
+		transform: translateY(-50%);
+		font-size: 20px;
+		text-align: center;
+		line-height: 40px;
+		width: 24px;
+		height: 40px;
+		border-radius: 4px;
+		background-color: rgba(0,0,0,0.3);
+		color: #777;
+		cursor:pointer;	
+	}/*左右*/
+	.left{left: 10px;}
+	.right{right: 10px;}
+	.left:hover{color: #fff}
+	.right:hover{color: #fff}
+	/*---------------------------*/
+	.dh-enter-active, .dh-leave-active {
+		transition: opacity 1s;
+	}
+	.dh-enter, .dh-leave-to{
+		opacity: 0;
+	}
+	/*---------------------------*/
+</style>
+<script type="text/javascript">
+	export default{
+		name:"lbt",
+		data(){
+			return{
+				tp:[
+					'https://www.yangqq.com/skin/blankgd/images/banner01.jpg',
+		          	'https://www.yangqq.com/skin/blankgd/images/banner02.jpg',
+		          	'https://www.yangqq.com/skin/blankgd/images/banner03.jpg',
+				],
+				width:800,
+				height:300,
+				cs:0,
+				play:null
+			}
+		},
+		computed:{
+			W(){
+				return (this.tp.length+2)*this.width+"px";
+			}/*获取总长度*/
+		},
+		methods:{
+			rightPlay(){
+				this.cs++;
+				if(this.cs===this.tp.length){
+					this.cs=0;
+				}
+			},
+			leftPlay(){
+				if(this.cs==0){
+					this.cs=this.tp.length;
+				}
+				this.cs--;
+			},
+			xzPlay(c){
+				this.cs=c;
+			},
+			autoPlay(){
+				this.play=setInterval(this.rightPlay,3000);
+			},
+			stop(){
+				clearInterval(this.play);
+			},
+			plauAgain(){
+
+			}
+		},
+		created(){
+			this.autoPlay();
+		}
+	}
+</script>
